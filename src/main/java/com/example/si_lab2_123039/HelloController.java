87e7792d8ce73d555bd1a995e9dcd251537abb8e
Stefan <stefanstojanovski180@gmail.com>
2023-05-31 23:41:18 +0200

    First commit

---
+++ src/main/java/com/example/si_lab2_123039/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.si_lab2_123039;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.si_lab2_123039 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.si_lab2_123039 to javafx.fxml;
+    exports com.example.si_lab2_123039;
+}
